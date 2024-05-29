cff0ad04a5303a8717e6356907f99e5dbcfe9e89
T4vIi02 <153662425+T4vIi02@users.noreply.github.com>
2024-05-29 16:43:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,53 +9,48 @@ function openModal(imageIndex) {
     // Set the content based on the clicked image
     switch (imageIndex) {
         case 1:
-            modalTitle.innerText = 'Title 1';
-            modalText.innerText = 'Detailed information about Image 1.';
+            modalTitle.innerText = 'Muscatele';
+            modalText.innerText = 'Pelargonium este un gen de plante florale, cunoscut popular sub numele de mușcate, care include circa 200 de specii. Aceste specii înfloresc din primăvară până în toamnă.';
             modalImage1.src = 'image1.jpg';
             modalImage2.src = 'image1_2.jpg';
             modalImage3.src = 'image1_3.jpg';
             break;
         case 2:
-            modalTitle.innerText = 'Title 2';
-            modalText.innerText = 'Detailed information about Image 2.';
+            modalTitle.innerText = 'Trandafirii';
+            modalText.innerText = 'Trandafirul este un gen de plante perene ornamentale din familia Rosaceae, originar din regiunile continentale și subtropicale ale emisferei nordice, cuprinzând peste 200 de specii de arbuști, deseori spinoși.';
             modalImage1.src = 'image2.jpg';
             modalImage2.src = 'image2_2.jpg';
             modalImage3.src = 'image2_3.jpg';
             break;
         case 3:
-            modalTitle.innerText = 'Title 3';
-            modalText.innerText = 'Detailed information about Image 3.';
+            modalTitle.innerText = 'Stanjeneii';
+            modalText.innerText = 'Stânjenel este denumirea comună a unor plante monocotiledonate din familia Iridaceae. Genul cuprinde aproximativ 260-300 de specii, în România – cca 42 de specii.';
             modalImage1.src = 'image3.jpg';
             modalImage2.src = 'image3_2.jpg';
             modalImage3.src = 'image3_3.jpg';
             break;
         case 4:
-            modalTitle.innerText = 'Title 4';
-            modalText.innerText = 'Detailed information about Image 4.';
+            modalTitle.innerText = 'Bujorii';
+            modalText.innerText = 'Bujorul românesc este o specie de bujor din familia Paeoniaceae prezentă în pădurile de câmpie sau la marginea acestora, în Dobrogea, Muntenia și sudul Moldovei.';
             modalImage1.src = 'image4.jpg';
             modalImage2.src = 'image4_2.jpg';
             modalImage3.src = 'image4_3.jpg';
             break;
         case 5:
-            modalTitle.innerText = 'Title 5';
-            modalText.innerText = 'Detailed information about Image 5.';
+            modalTitle.innerText = 'Crinii';
+            modalText.innerText = 'Crinul este numele popular al genului Lilium, un gen de plante cu flori erbacee ce cresc din bulbi. Cele mai multe specii sunt native emisferei nordice temperate. Ele cuprind un gen de circa 110 specii din familia crinului.';
             modalImage1.src = 'image5.jpg';
             modalImage2.src = 'image5_2.jpg';
             modalImage3.src = 'image5_3.jpg';
             break;
         case 6:
-            modalTitle.innerText = 'Title 6';
-            modalText.innerText = 'Detailed information about Image 6.';
+            modalTitle.innerText = 'Narcisele';
+            modalText.innerText = 'Narcisa este un gen originar din nordul Africii, regiuni mediteraneene și Europa Centrală, circa 32 specii erbacee, bulboase.';
             modalImage1.src = 'image6.jpg';
             modalImage2.src = 'image6_2.jpg';
             modalImage3.src = 'image6_3.jpg';
             break;
-        default:
-            modalTitle.innerText = 'Title';
-            modalText.innerText = 'Detailed information about the selected image.';
-            modalImage1.src = '';
-            modalImage2.src = '';
-            modalImage3.src = '';
+
     }
 
     modal.style.display = 'block';
